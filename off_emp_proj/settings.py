d139837a78bc77cd9994563b1a062ddbb44249df
Pritam Bhattacharya <83444990+fusionpritam@users.noreply.github.com>
2022-08-03 14:24:27 +0530

    Update settings.py

--- off_emp_proj/settings.py
+++ off_emp_proj/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d9u)w&jdxqeb6b*wo)x@&)#ihtvxt-a7ume*itimk6akd7ar$)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
